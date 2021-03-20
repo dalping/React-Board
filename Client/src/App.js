f09512fdc7dd5c97c57d097d1271fe49081c3763
Dalping <74459925+Bokyung626@users.noreply.github.com>
2021-03-20 16:13:21 +0900

    Test Api

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      Hello React!
-    </div>
-  );
+class App extends Component {
+
+  state = { //변경될 수 있는 데이터이므로 state변수 초기화 //state값이 바뀔 때마다 render함수가 실행된다.
+    board : ''
+  }
+
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({board: res})) //body 데이터를 받아와서 state변수 내용 변경
+    .catch(err => console.log(err)); //예외처리
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/board'); //위 경로에 접근해서
+    const body = await response.json(); // 결과를 변수 body에 넣어라
+    console.log(body);
+    return body;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello React!
+        {this.state.board ? this.state.board[0].name :''}
+      </div>
+    );
+  }
 }
+
+/*
+this.state.board.map(c=>{
+  return (<Board key={c.id} id={c.id} name={c.name} title={c.title} content={c.content}/> );
+})
+*/
 
 export default App;
--- server.js
+++ server.js
@@ -1,13 +1,33 @@
+const fs = require('fs');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.unsubscribe(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/board', (req,res) => {
+    res.send([
+       {
+           'id': 1,
+           'name' : "꼬부기",
+           'title': "안녕",
+           'content': "반갑습니다."
+       } ,
+       {
+        'id': 2,
+        'name' : "아기감자",
+        'title': "로미니",
+        'content': "귀여웡."
+        },
+        {
+            'id': 3,
+            'name' : "하기싫다",
+            'title': "왜하는겨",
+            'content': "짜증나"
+        } 
+     ]);
 });
 
 app.listen(port,()=>console.log(`Listen on port ${port}`));
